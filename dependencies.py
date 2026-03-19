356dc0497d7c68b55606fc711e4e947388f68020
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2026-03-19 11:32:52 -0300

    feat: Session manager

---
+++ dependencies.py
@@ -0,0 +1,13 @@
+from models import db
+from sqlalchemy.orm import sessionmaker, Session
+
+def get_session():
+    """
+        Get a db session and returns it
+    """
+    try:
+        Session = sessionmaker(bind=db)
+        session = Session()
+        yield session ## Yield sends a return with that result without exiting the function
+    finally:
+        session.close()
