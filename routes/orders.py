1bf54319742dd055cfb633f1b3891e2b74e3d8f5
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2026-03-29 17:11:22 -0300

    feat: rota pedidos adicionada

--- routes/orders.py
+++ routes/orders.py
@@ -1,10 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from schemas import PedidoSchema
+from dependencies import Session, pegar_sessao
+from models import Pedido
 
 order_router = APIRouter(prefix="/pedidos", tags=["pedidos"])
 
 @order_router.get("/")
-def get_orders():
+async def pedidos():
     """
         Orders Route
     """
-    return {"tobas":"ebas"}
+    return {"rota":"pedidos"}
+
+@order_router.post("/pedido")
+async def criar_pedido(pedido_schema: PedidoSchema, session: Session = Depends(pegar_sessao)):
+    novo_pedido = Pedido(usuario=pedido_schema.usuario)
+
+    session.add(novo_pedido)
+    session.commit()
+    return {"mensagem": f"Pedido criado com sucesso! ID do pedido: {novo_pedido.id}"}
+    
