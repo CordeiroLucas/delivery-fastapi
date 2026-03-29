2234c3b6fb68b973037e4588920c4b83e9a24254
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2026-03-29 17:29:12 -0300

    feat: basic login logic made

--- routes/auth.py
+++ routes/auth.py
@@ -2,9 +2,13 @@ from fastapi import APIRouter, Depends, HTTPException, status
 from models import Usuario
 from dependencies import pegar_sessao, Session
 from main import bcrypt_context
-from schemas import UsuarioSchema
+from schemas import UsuarioSchema, LoginSchema
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
+
+def criar_token(email):
+    token = f"dAosjdaoiO9apkAOUNbuAjrNOisdo{email}"
+    return token
 
 @auth_router.get("/")
 async def usuarios():
@@ -18,7 +22,7 @@ async def registrar_usuario(usuario_schema: UsuarioSchema, session: Session = De
     usuario = session.query(Usuario).filter(Usuario.email == usuario_schema.email).first()
 
     if usuario:
-        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Usuário já Cadastrado!")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Usuário já Cadastrado!")
 
     senha_critpografada = bcrypt_context.hash(usuario_schema.senha)
 
@@ -28,3 +32,20 @@ async def registrar_usuario(usuario_schema: UsuarioSchema, session: Session = De
 
     return {"message":f"{usuario_schema.nome}, {usuario_schema.email}, {senha_critpografada} - Registrado com sucesso!"}
 
+@auth_router.post("/login")
+async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
+    usuario = session.query(Usuario).filter(Usuario.email == login_schema.email).first()
+
+    if not usuario:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Usuario não encontrado")
+    
+    access_token = criar_token(usuario.id)
+
+    return {
+        "Access-Token" : access_token,
+        "Token-Type" : "Bearer"
+    }
+
+    # headers = {
+    #     "Access-Token" : f"Bearer {}"
+    # }
