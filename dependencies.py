6179d49bbedc58a3df7b7753791116372a47cf8c
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2026-04-05 23:40:21 -0300

    feat: basic auth validation and jwt token without oauth2

--- dependencies.py
+++ dependencies.py
@@ -1,5 +1,8 @@
-from models import db
+from models import db, Usuario
 from sqlalchemy.orm import sessionmaker, Session
+from fastapi import Depends, HTTPException, status
+from jose import jwt, JWTError
+from main import SECRET_KEY, ALGORITHM
 
 def pegar_sessao():
     """
@@ -11,3 +14,17 @@ def pegar_sessao():
         yield session ## Yield sends a return with that result without exiting the function
     finally:
         session.close()
+
+def verificar_token(token, session: Session = Depends(pegar_sessao)):
+    try:
+        dic_info = jwt.decode(token, SECRET_KEY, ALGORITHM)
+        id_usuario = dic_info.get("sub", 0)
+
+    except JWTError:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Acesso Negado, Token Expirado ou Inválido")
+    
+    usuario = session.query(Usuario).filter(Usuario.id == id_usuario).first()
+    if not usuario:
+        raise HTTPException(status.HTTP_401_UNAUTHORIZED, detail="Acesso Inválido")
+
+    return usuario
--- routes/auth.py
+++ routes/auth.py
@@ -1,14 +1,34 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from models import Usuario
-from dependencies import pegar_sessao, Session
-from main import bcrypt_context
+from dependencies import pegar_sessao, Session, verificar_token
+from main import bcrypt_context, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY
 from schemas import UsuarioSchema, LoginSchema
+from jose import jwt, JWTError
+from datetime import datetime, timedelta, timezone
 
 auth_router = APIRouter(prefix="/auth", tags=["auth"])
 
-def criar_token(email):
-    token = f"dAosjdaoiO9apkAOUNbuAjrNOisdo{email}"
-    return token
+def criar_token(id_usuario:int, duracao_token=timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)):
+    data_expiracao = datetime.now(timezone.utc) + duracao_token
+    dic_info = {
+        "sub": id_usuario,
+        "exp": data_expiracao
+    }
+
+    jwt_codificado = jwt.encode(dic_info, SECRET_KEY, ALGORITHM)
+
+    return jwt_codificado
+
+def autenticar_usuario(email, senha, session: Session):
+    usuario = session.query(Usuario).filter(Usuario.email == email).first()
+
+    if not usuario:
+        return False
+    
+    elif not bcrypt_context.verify(senha, usuario.senha):
+        return False
+
+    return usuario
 
 @auth_router.get("/")
 async def usuarios():
@@ -34,18 +54,27 @@ async def registrar_usuario(usuario_schema: UsuarioSchema, session: Session = De
 
 @auth_router.post("/login")
 async def login(login_schema: LoginSchema, session: Session = Depends(pegar_sessao)):
-    usuario = session.query(Usuario).filter(Usuario.email == login_schema.email).first()
+
+    usuario = autenticar_usuario(login_schema.email, login_schema.senha, session)
 
     if not usuario:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Usuario não encontrado")
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Usuario Não Encontrado ou Credenciais Incorreto!")
     
+    access_token = criar_token(usuario.id)
+    refresh_token = criar_token(usuario.id, duracao_token=timedelta(days=7))
+
+    return {
+        "access-token" : access_token,
+        "refresh-token": refresh_token,
+        "token-type" : "Bearer"
+    }
+
+@auth_router.get("/refresh")
+async def use_refresh_token(usuario: Usuario = Depends(verificar_token)):
+
     access_token = criar_token(usuario.id)
 
     return {
-        "Access-Token" : access_token,
-        "Token-Type" : "Bearer"
+        "access-token" : access_token,
+        "token-type" : "Bearer"
     }
-
-    # headers = {
-    #     "Access-Token" : f"Bearer {}"
-    # }
